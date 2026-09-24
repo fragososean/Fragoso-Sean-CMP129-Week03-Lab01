64d770cd19e2f89c0f8da5160724a12007068ddb
fragoso.sean <fragoso.sean@student.ccm.edu>
2026-09-24 18:33:41 -0400

    Complete Week 3 Lab 1

--- Date.java
+++ Date.java
@@ -1,3 +1,47 @@
 public class Date {
-    
+    //setMonth, getMonth(int)
+    //setMonth, getMonth(String)
+    //setDay, getDay
+    //setYear, getYear
+
+    private int Day, Monthi, Year;
+    private String MonthS;
+
+    public void setDay(int d){
+        Day = d;
+    }
+
+    public int getDay(){
+        return Day;
+    }
+
+    public void setMonthi(int m){
+        Monthi = m;
+    }
+
+    public int getMonthi(){
+        return Monthi;
+    }
+
+    public void setMonthS(String M){
+        MonthS = M;
+    }
+
+    public String getMonthS(){
+        return MonthS;
+    }
+
+    public void setYear(int y){
+        Year = y;
+    }
+
+    public int getYear(){
+        return Year;
+    }
+
+    public void getDate(){
+        System.out.println(getMonthi()+"/"+getDay()+"/"+getYear());
+        System.out.println(getMonthS()+" "+getDay()+", "+getYear());
+        System.out.println(getDay()+" "+getMonthS()+" "+getYear());
+    }
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,108 @@
+import java.util.Scanner;
 public class DateTest {
-    
+    public static void main (String[]args){
+        Scanner input = new Scanner (System.in);
+        Date d1 = new Date();
+        int d, m, y;
+        String M = null;
+
+        System.out.println("Use only numerics in your input.");
+
+        System.out.print("Enter the Month: ");
+        m = input.nextInt();
+        while(m<1 || m>12){
+            System.out.print("That number is not accepted.\nEnter the Month: ");
+            m = input.nextInt();
+        }
+
+        d1.setMonthi(m);
+
+        if(m == 1){
+            M = "January";
+        }
+
+        if(m == 2){
+            M = "February";
+        }
+
+        if(m == 3){
+            M = "March";
+        }
+
+        if(m == 4){
+            M = "April";
+        }
+
+        if(m == 5){
+            M = "May";
+        }
+
+        if(m == 6){
+            M = "June";
+        }
+
+        if(m == 7){
+            M = "July";
+        }
+
+        if(m == 8){
+            M = "August";
+        }
+
+        if(m == 9){
+            M = "September";
+        }
+
+        if(m == 10){
+            M = "October";
+        }
+
+        if(m == 11){
+            M = "November";
+        }
+
+        if(m == 12){
+            M = "December";
+        }
+
+        d1.setMonthS(M);
+
+        System.out.print("Enter the Day: ");
+        d = input.nextInt();
+        while(d<1 || d>31){
+            System.out.print("That number is not accepted.\nEnter the Day: ");
+            d = input.nextInt();
+        }
+            while(d>29 && m == 2){
+                System.out.print("That number is not accepted.\nEnter the Day: ");
+                d = input.nextInt();
+            }
+            while(d>30 && m == 4){
+                System.out.print("That number is not accepted.\nEnter the Day: ");
+                d = input.nextInt();
+            }
+
+            while(d>30 && m == 6){
+                System.out.print("That number is not accepted.\nEnter the Day: ");
+                d = input.nextInt();
+            }
+            
+            while(d>30 && m == 9){
+                System.out.print("That number is not accepted.\nEnter the Day: ");
+                d = input.nextInt();
+            }
+
+            while(d>30 && m == 11){
+                System.out.print("That number is not accepted.\nEnter the Day: ");
+                d = input.nextInt();
+            }
+        d1.setDay(d);
+
+        System.out.print("Enter the Year: ");
+        y = input.nextInt();
+        d1.setYear(y);
+
+        d1.getDate();
+
+    }
 }
